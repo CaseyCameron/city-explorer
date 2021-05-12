a44fba3be2b68bf88c52c8f7d5d866e1d7eca41e
Casey Cameron <mcasey.cameron@gmail.com>
2021-05-12 14:08:07 -0700

    location test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,116 +1,91 @@
-import app from '../lib/app.js';
-import supertest from 'supertest';
-import client from '../lib/client.js';
-import { execSync } from 'child_process';
+import movieData from '../data/movies';
+import { locationData }  from '../data/location';
+import yelpData from '../data/yelp';
+import weatherData from '../data/weather';
+import { formatMovies, formatLocation } from '../lib/munge-utils';
 
-const request = supertest(app);
+describe('API Data Munging', () => {
 
-describe('API Routes', () => {
-
-  beforeAll(() => {
-    execSync('npm run setup-db');
-  });
-
-  afterAll(async () => {
-    return client.end();
-  });
-
-  const expectedCats = [
+  const expectedMovies = [
     {
-      id: expect.any(Number),
-      name: 'Felix',
-      type: 'Tuxedo',
-      url: 'cats/felix.png',
-      year: 1892,
-      lives: 3,
-      isSidekick: false
+      "title": "Star Wars",
+      "description": "Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.",
+      "image_url": "/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg",
+      "released": "1977-05-25",
+      "average_rating": 8.2
     },
     {
-      id: expect.any(Number),
-      name: 'Garfield',
-      type: 'Orange Tabby',
-      url: 'cats/garfield.jpeg',
-      year: 1978,
-      lives: 7,
-      isSidekick: false
+      "title": "Star Wars: The Rise of Skywalker",
+      "description": "The surviving Resistance faces the First Order once again as the journey of Rey, Finn and Poe Dameron continues. With the power and knowledge of generations behind them, the final battle begins.",
+      "image_url": "/db32LaOibwEliAmSL2jjDF6oDdj.jpg",
+      "released": "2019-12-18",
+      "average_rating": 6.5
     },
     {
-      id: expect.any(Number),
-      name: 'Duchess',
-      type: 'Angora',
-      url: 'cats/duchess.jpeg',
-      year: 1970,
-      lives: 9,
-      isSidekick: false
-    },
-    {
-      id: expect.any(Number),
-      name: 'Stimpy',
-      type: 'Manx',
-      url: 'cats/stimpy.jpeg',
-      year: 1990,
-      lives: 1,
-      isSidekick: true
-    },
-    {
-      id: expect.any(Number),
-      name: 'Sylvester',
-      type: 'Tuxedo',
-      url: 'cats/sylvester.jpeg',
-      year: 1945,
-      lives: 1,
-      isSidekick: true
-    },
-    {
-      id: expect.any(Number),
-      name: 'Tigger',
-      type: 'Orange Tabby',
-      url: 'cats/tigger.jpeg',
-      year: 1928,
-      lives: 8,
-      isSidekick: false
-    },
-    {
-      id: expect.any(Number),
-      name: 'Hello Kitty',
-      type: 'Angora',
-      url: 'cats/hello-kitty.jpeg',
-      year: 1974,
-      lives: 9,
-      isSidekick: false
-    },
-    {
-      id: expect.any(Number),
-      name: 'Hobbs',
-      type: 'Orange Tabby',
-      url: 'cats/hobbs.jpeg',
-      year: 1985,
-      lives: 6,
-      isSidekick: true
+      "title": "Star Wars: The Last Jedi",
+      "description": "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
+      "image_url": "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+      "released": "2017-12-13",
+      "average_rating": 6.9
     }
   ];
 
-  // If a GET request is made to /api/cats, does:
-  // 1) the server respond with status of 200
-  // 2) the body match the expected API data?
-  it('GET /api/cats', async () => {
-    // act - make the request
-    const response = await request.get('/api/cats');
+  const expectedLocation = [
+    {
+      "formatted_query": "Oakland, Alameda County, California, USA",
+      "latitude": "37.8044557",
+      "longitude": "-122.2713563"
+    }
+  ];
 
-    // was response OK (200)?
-    expect(response.status).toBe(200);
+  const expectedWeather = [
+    {
+      "forecast": "Partly cloudy until afternoon.",
+      "time": "Mon Jan 01 2001"
+    },
+    {
+      "forecast": "Mostly cloudy in the morning.",
+      "time": "Tue Jan 02 2001"
+    }
+  ];
 
-    // did it return the data we expected?
-    expect(response.body).toEqual(expectedCats);
+  const expectedYelp = [
+    {
+      "name": "Pike Place Chowder",
+      "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/ijju-wYoRAxWjHPTCxyQGQ/o.jpg",
+      "price": "$$   ",
+      "rating": "4.5",
+      "url": "https://www.yelp.com/biz/pike-place-chowder-seattle?adjust_creative=uK0rfzqjBmWNj6-d3ujNVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=uK0rfzqjBmWNj6-d3ujNVA"
+    },
+    {
+      "name": "Umi Sake House",
+      "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/c-XwgpadB530bjPUAL7oFw/o.jpg",
+      "price": "$$   ",
+      "rating": "4.0",
+      "url": "https://www.yelp.com/biz/umi-sake-house-seattle?adjust_creative=uK0rfzqjBmWNj6-d3ujNVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=uK0rfzqjBmWNj6-d3ujNVA"
+    }
+  ];
 
+  it('munges movie data', async () => {
+    // arrange
+    // expected is in variable above
+    // movieData is imported from file
+
+    // act 
+    const output = formatMovies(movieData);
+
+    // assert
+    expect(output).toEqual(expectedMovies);
+  });
+  
+  it('munges location data', async () => {
+
+    const output = formatLocation(locationData);
+    console.log(locationData);
+    // assert
+    expect(output).toEqual(expectedLocation);
   });
 
-  // If a GET request is made to /api/cats/:id, does:
-  // 1) the server respond with status of 200
-  // 2) the body match the expected API data for the cat with that id?
-  test('GET /api/cats/:id', async () => {
-    const response = await request.get('/api/cats/2');
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual(expectedCats[1]);
-  });
+
+
 });
---
+++ data/weather.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    "forecast": "Partly cloudy until afternoon.",
+    "time": "Mon Jan 01 2001"
+  },
+  {
+    "forecast": "Mostly cloudy in the morning.",
+    "time": "Tue Jan 02 2001"
+  }
+]
