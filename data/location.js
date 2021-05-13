0c6b7b7d8e9297233a3c5df126d7f08d96a8cf78
Casey Cameron <mcasey.cameron@gmail.com>
2021-05-12 17:16:32 -0700

    Something wrong with the city munging

--- data/location.js
+++ data/location.js
@@ -1,21 +1,21 @@
 export const locationData = [
   {
-  "place_id": "236402977",
-  "licence": "https://locationiq.com/attribution",
-  "osm_type": "relation",
-  "osm_id": "2833530",
-  "boundingbox": [
-  "37.6336763",
-  "37.8854257",
-  "-122.355881",
-  "-122.1144203"
-  ],
-  "lat": "37.8044557",
-  "lon": "-122.2713563",
-  "display_name": "Oakland, Alameda County, California, USA",
-  "class": "place",
-  "type": "city",
-  "importance": 0.741796254253183,
-  "icon": "https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png"
+    "place_id": "236402977",
+    "licence": "https://locationiq.com/attribution",
+    "osm_type": "relation",
+    "osm_id": "2833530",
+    "boundingbox": [
+    "37.6336763",
+    "37.8854257",
+    "-122.355881",
+    "-122.1144203"
+    ],
+    "lat": "37.8044557",
+    "lon": "-122.2713563",
+    "display_name": "Oakland, Alameda County, California, USA",
+    "class": "place",
+    "type": "city",
+    "importance": 0.741796254253183,
+    "icon": "https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png"
   }
 ];
